36908e8edb3b0aecc72aca965bdc1855629a5598
David Browning <david.a.browning@gmail.com>
2025-04-16 19:35:02 +0200

    Add first passing test.

---
+++ Library.Tests/TestHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests
+{
+    internal static class TestHelper
+    {
+        private static void PrintSuccess(string title)
+        {
+            Console.WriteLine($"Success: {title}");
+        }
+        private static void PrintFailure(string title, object expected, object actual)
+        {
+            Console.WriteLine($"Failure: {title} | Expected: {expected} | Actual: {actual}");
+        }
+        public static void AssertEquals(string title, object expected, object actual)
+        {
+            if (Equals(expected, actual))
+                PrintSuccess(title);
+            else
+                PrintFailure(title, expected, actual);
+        }
+    }
+}
--- Library.Tests/TestRunner.cs
+++ Library.Tests/TestRunner.cs
@@ -1,10 +1,24 @@
-﻿namespace Library.Tests
+﻿using EFCoreLibrary.DataAccess;
+
+namespace Library.Tests
 {
     public class TestRunner
     {
         public void RunTests()
         {
+            string title;
+            int initialCount, finalCount;
+            LibraryDbContext dbCtx = new LibraryDbContext();
+
             Console.WriteLine("Running tests...");
+
+            title = "Books count increases by 1 when book added to db";
+            initialCount = dbCtx.Books.Count();
+            dbCtx.Add(new Book() { Title = "TestBook" });
+            dbCtx.SaveChanges();
+            finalCount = dbCtx.Books.Count();
+            TestHelper.AssertEquals(title, 1, finalCount - initialCount);
+
         }
     }
 }
